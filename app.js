9388f95e7b4ab42ce18e7daa20df025e9a0a8f91
kavithav-cloud <kavithav.2111@gmail.com>
2026-05-11 12:40:23 +0530

    Change server port from 3000 to 8080

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express =  require('express');
 const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
-const port = 3000;  
+const port = 8080;  
 
 app.get('/', (req, res) => {
   res.send(`Hello World! ${process.env.environment}`);
