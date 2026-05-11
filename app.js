97e918e2da01c63dc51262bc5522019c75dc7054
kavithav-cloud <kavithav.2111@gmail.com>
2026-05-11 13:39:41 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -2,10 +2,22 @@ const express =  require('express');
 const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
-const port = 8080;  
+const QueryResults = require('./db.js');
+
+const port = 3000;  
 
 app.get('/', (req, res) => {
-  res.send(`Hello World!! ${process.env.environment}`);
+  res.send(`Hello World! Environment: ${process.env.environment}`);
+  
+});
+app.get('/users', async (req, res) => {
+  try {
+    const users = await QueryResults.queryresults();
+    res.json(users);
+  } catch (err) {
+    console.error('Error fetching users:', err.stack);
+    res.status(500).send('Error fetching users');
+  }
 });
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
---
+++ db.js
@@ -0,0 +1,29 @@
+const poll = require('pg-pool');
+const dotenv = require('dotenv');
+dotenv.config();        
+
+const pool = new poll({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,  
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+});
+
+const query = 'SELECT * FROM users';
+
+const queryresults = () => {
+    return new Promise((resolve, reject) => {
+        pool.query(query, (err, res) => {
+            if (err) {
+                console.error('Error executing query', err.stack);
+                reject(err);
+            } else {
+                resolve(res.rows);
+            }
+        });
+    });
+};
+module.exports = { queryresults };
+ 
+
